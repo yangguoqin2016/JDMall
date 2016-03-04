4522c1e2028d1248a6e685563d7ed44839fcb29f
Ke Liu <lk199044@gmail.com>
2016-03-04 14:24:28 +0800

    ...

--- jdmall/src/main/java/com/onlyone/jdmall/SplashActivity.java
+++ jdmall/src/main/java/com/onlyone/jdmall/SplashActivity.java
@@ -1,7 +1,7 @@
 package com.onlyone.jdmall;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +9,6 @@ public class SplashActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		System.out.println("hello");
 	}
 }

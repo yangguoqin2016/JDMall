e16f0d4c1b194ef1eae288d63e665020319f64e9
yangguoqin <277027133@qq.com>
2016-03-04 14:24:42 +0800

    杨国钦,,我在测试上交

--- jdmall/src/main/java/com/onlyone/jdmall/SplashActivity.java
+++ jdmall/src/main/java/com/onlyone/jdmall/SplashActivity.java
@@ -3,11 +3,16 @@ package com.onlyone.jdmall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.lang.System;
+
 public class SplashActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+
+
+		System.out.print("Ñî¹úÇÕÕıÔÚ²âÊÔ");
 	}
 }

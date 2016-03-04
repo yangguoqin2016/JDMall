126f785fb615b6a7cb463d785ce80164b8c5c671
Ke Liu <lk199044@gmail.com>
2016-03-04 14:32:38 +0800

    add class MyApplication

--- jdmall/src/main/java/com/onlyone/jdmall/SplashActivity.java
+++ jdmall/src/main/java/com/onlyone/jdmall/SplashActivity.java
@@ -1,7 +1,7 @@
 package com.onlyone.jdmall;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,7 +11,5 @@ public class SplashActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_splash);
 		System.out.println("hello");
 
-
-		System.out.print("��������ڲ���");
 	}
 }

f8970c870b0bd0309cbdbbbde56fc3843bfb5999
Ke Liu <lk199044@gmail.com>
2016-03-05 20:35:20 +0800

    ListView SuperBaseAdapter抽取与BaseHolder抽取.包含上滑加载更多功能.

--- jdmall/src/main/java/com/onlyone/jdmall/fragment/HotProductFragment.java
+++ jdmall/src/main/java/com/onlyone/jdmall/fragment/HotProductFragment.java
@@ -1,24 +1,18 @@
 package com.onlyone.jdmall.fragment;
 
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.onlyone.jdmall.R;
+import com.onlyone.jdmall.adapter.SuperBaseAdapter;
 import com.onlyone.jdmall.bean.HotProductBean;
 import com.onlyone.jdmall.constance.Url;
-import com.onlyone.jdmall.pager.LoadListener;
+import com.onlyone.jdmall.holder.BaseHolder;
+import com.onlyone.jdmall.holder.HotProductHolder;
 import com.onlyone.jdmall.utils.ResUtil;
-import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -29,13 +23,25 @@ import java.util.List;
  * 创建时间: 2016/3/5 12:31
  * 描述:用于展示热门单品的Fragment
  */
-public class HotProductFragment extends BaseFragment<List<HotProductBean.ProductBean>> {
+public class HotProductFragment extends SuperBaseFragment<List<HotProductBean.ProductBean>> {
 
     ListView mHotProductListView;
+    /**
+     * 当前加载的页数
+     */
+    private int mCurPageNum = 1;
+    /**
+     * 加载更多的数据
+     */
+    private List<HotProductBean.ProductBean> mLoadMoreDatas;
+    /**
+     * 加载跟多时的异常
+     */
+    private VolleyError                      mLoadError;
 
     @Override
-    protected void refreshSuccessView(List<HotProductBean.ProductBean> data) {
-        mHotProductListView.setAdapter(new HotProductAdapter(data));
+    protected void refreshSuccessView(List<HotProductBean.ProductBean> datas) {
+        mHotProductListView.setAdapter(new HotProductAdapter(datas));
     }
 
     @Override
@@ -46,93 +52,48 @@ public class HotProductFragment extends BaseFragment<List<HotProductBean.Product
     }
 
     @Override
-    protected void loadData(final LoadListener<List<HotProductBean.ProductBean>> listener) {
-        //网络请求热门单品数据
-        //http://localhost:8080/market/hotproduct?page=1&pageNum=15&orderby=saleDown
-        //上拉加载更多改变页数即可
-        String url = Url.ADDRESS_SERVER+"/hotproduct?page=1&pageNum=15&orderby=saleDown";
-        StringRequest request = new StringRequest(url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String s) {
-                Gson gson = new Gson();
-                HotProductBean bean = gson.fromJson(s, HotProductBean.class);
-                List<HotProductBean.ProductBean> datas = bean.productList;
-                listener.onSuccess(datas);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                listener.onError(volleyError);
-            }
-        });
-
-        Volley.newRequestQueue(ResUtil.getContext()).add(request);
+    protected String getUrl() {
+        String url = Url.ADDRESS_SERVER + "/hotproduct?page=" + mCurPageNum + "&pageNum=15&orderby=saleDown";
+        return url;
     }
+
 
     @Override
     protected void handleError(Exception e) {
         Toast.makeText(ResUtil.getContext(), e.toString(), Toast.LENGTH_SHORT).show();
     }
 
-    private class HotProductAdapter extends BaseAdapter {
-        List<HotProductBean.ProductBean> mDatas;
-        public HotProductAdapter(List<HotProductBean.ProductBean> data) {
-            mDatas = data;
+    @Override
+    protected List<HotProductBean.ProductBean> parseJson(String jsonStr) {
+        Gson gson = new Gson();
+        HotProductBean bean = gson.fromJson(jsonStr, HotProductBean.class);
+        List<HotProductBean.ProductBean> datas = bean.productList;
+        return datas;
+    }
+
+    /**
+     * 热门品牌ListView展示数据所用的Adapter
+     */
+    private class HotProductAdapter extends SuperBaseAdapter<HotProductBean.ProductBean> {
+
+        public HotProductAdapter(List<HotProductBean.ProductBean> datas) {
+            super(datas);
         }
 
         @Override
-        public int getCount() {
-            if(mDatas != null){
-                return mDatas.size();
-            }
-            return 0;
+        public BaseHolder initSpecialHolder() {
+            HotProductHolder holder = new HotProductHolder();
+            return holder;
         }
 
+
         @Override
-        public Object getItem(int position) {
-            if(mDatas != null){
-                mDatas.get(position);
-            }
+        protected List<HotProductBean.ProductBean> doLoadMore() throws Exception {
+            mCurPageNum = mCurPageNum + 1;
+            String url = Url.ADDRESS_SERVER + "/hotproduct?page=" + mCurPageNum + "&pageNum=15&orderby=saleDown";
+            //网络请求数据,该方法已属异步执行
+
             return null;
         }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ViewHolder holder = null;
-            if(convertView == null){
-                holder = new ViewHolder();
-                convertView = View.inflate(ResUtil.getContext(),R.layout.hot_product_item,null);
-                holder.ivPic = (ImageView) convertView.findViewById(R.id.hot_product_item_pic);
-                holder.tvName = (TextView) convertView.findViewById(R.id.hot_product_item_name);
-                holder.tvPrice = (TextView) convertView.findViewById(R.id.hot_product_item_price);
-                holder.tvMarketPrice = (TextView) convertView.findViewById(R.id.hot_product_item_market_price);
-                convertView.setTag(holder);
-            }else{
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            HotProductBean.ProductBean hotProductBean = mDatas.get(position);
-            holder.tvName.setText(hotProductBean.name);
-            holder.tvMarketPrice.setText(hotProductBean.marketPrice+"");
-            holder.tvPrice.setText(hotProductBean.price+"");
-
-            String url = Url.ADDRESS_SERVER+hotProductBean.pic;
-            Picasso.with(ResUtil.getContext()).load(url).into(holder.ivPic);
-
-            return convertView;
-        }
-
-        class ViewHolder{
-            ImageView ivPic;
-            TextView tvName;
-            TextView tvPrice;
-            TextView tvMarketPrice;
-        }
-
     }
 }

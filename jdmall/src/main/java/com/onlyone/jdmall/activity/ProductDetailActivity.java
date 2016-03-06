c8cd8435d303744de340aea380d0324fcca849de
Ke Liu <lk199044@gmail.com>
2016-03-06 17:02:00 +0800

    [modify]详情界面UI

--- jdmall/src/main/java/com/onlyone/jdmall/activity/ProductDetailActivity.java
+++ jdmall/src/main/java/com/onlyone/jdmall/activity/ProductDetailActivity.java
@@ -3,10 +3,13 @@ package com.onlyone.jdmall.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.onlyone.jdmall.R;
+
 public class ProductDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
     }
 }

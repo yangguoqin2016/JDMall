21850801455fb95406e3bf23e31db9c29bde9ef3
王涛 <136745162@qq.com>
2016-03-07 10:24:51 +0800

    [add]添加countdownview的依赖

--- jdmall/src/main/java/com/onlyone/jdmall/fragment/home/HomeFragment.java
+++ jdmall/src/main/java/com/onlyone/jdmall/fragment/home/HomeFragment.java
@@ -307,6 +307,7 @@ public class HomeFragment extends BaseFragment<Object> implements ViewPager.OnPa
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // TODO: inflate a fragment view
         View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        ButterKnife.bind(this, rootView);
         return rootView;
     }
 

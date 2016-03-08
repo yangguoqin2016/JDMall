aa46be3713596aefa872ff89ea28c0b220ec727c
zhangbo <111111>
2016-03-08 21:53:58 +0800

    我的订单界面的实现

---
+++ jdmall/src/main/java/com/onlyone/jdmall/bean/MyIndentBean.java
@@ -0,0 +1,16 @@
+package com.onlyone.jdmall.bean;
+
+import java.util.List;
+
+/**
+ * @项目名: JDMall
+ * @包名: com.onlyone.jdmall.bean
+ * @创建者: Administrator
+ * @创建时间: 2016/3/8 14:07
+ * @描述: ${TODO}
+ */
+public class MyIndentBean {
+    public String                  response;
+    public List<IndentMesListBean> orderList;
+
+}

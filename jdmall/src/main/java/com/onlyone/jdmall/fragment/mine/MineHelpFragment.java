f72b31a018d27337406d8620372becb9348df3cf
Jeremy <1264897620@qq.com>
2016-03-09 11:20:17 +0800

    [modify]fragment修改为BaseFragment

--- jdmall/src/main/java/com/onlyone/jdmall/fragment/mine/MineHelpFragment.java
+++ jdmall/src/main/java/com/onlyone/jdmall/fragment/mine/MineHelpFragment.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -196,7 +195,7 @@ public class MineHelpFragment extends BaseFragment<Object> implements View.OnCli
      * @param fragment
      * @param tag
      */
-    private void changeFragment(Fragment fragment, String tag) {
+    private void changeFragment(BaseFragment fragment, String tag) {
         /*FragmentManager manager = mMainActivity.getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.add(R.id.fl_content_container, fragment, tag);
